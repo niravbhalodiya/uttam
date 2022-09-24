50d32be98ae80816ff868d23908851e1ac34c4e5
Hritik Adhikari <hritikadhikari05@gmail.com>
2022-09-24 16:05:27 +0530

    changed folder structure

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require('express-session');
+const MongoDBStore = require("connect-mongodb-session")(session);
 require("dotenv").config();
 
 const app = express();
@@ -10,12 +12,45 @@ const app = express();
 //Local Imports
 const userRoutes = require("./router/userRoutes")
 const adminRoutes = require("./router/adminRoutes")
+const User = require("./models/user");
+
+const store = new MongoDBStore({
+    uri: process.env.MONGO_URL,
+    collection: "sessions",
+});
+
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.use("/api/user",userRoutes);
+
+//Implementation of session
+app.use(
+    session({
+        secret: "Its complicated",
+        resave: false,
+        saveUninitialized: false,
+        store: store,
+    })
+);
+app.use((req, res, next) => {
+    //   console.log();
+    if (!req.session.user) {
+        return next();
+    }
+    User.findById(req.session.user._id)
+        .then((user) => {
+            req.user = user;
+            next();
+        })
+        .catch((err) => console.log(err));
+});
+
+
+
+//Routes
+app.use("/api/user", userRoutes);
 // app.use("/api/admin",adminRoutes);
 
 

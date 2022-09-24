54b25e418026e5a1a5861cfb860bdc8346d659ab
Femil32 <femilsavaliya555@gmail.com>
2022-09-24 15:17:54 +0530

    update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    plugins: [
+        react({
+            namedExports: {
+                'scheduler': ['unstable_runWithPriority', 'unstable_LowPriority']
+            }
+        }),
+        // viteCommonjs({
+        //     namedExports: {
+        //         'node_modules/formik/node_modules/scheduler/index.js': ['unstable_runWithPriority'],
+        //     }
+        // })
+    ]
 })
